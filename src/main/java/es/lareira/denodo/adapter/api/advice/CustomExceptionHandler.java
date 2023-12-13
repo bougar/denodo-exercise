a946890da79f07835aef11c65748c350ec9f3c66
Carlos Bouzón García <carlos.bouzon.garcia@gmail.com>
2023-12-14 00:57:28 +0100

    #13 improve exception handling

---
+++ src/main/java/es/lareira/denodo/adapter/api/advice/CustomExceptionHandler.java
@@ -0,0 +1,40 @@
+package es.lareira.denodo.adapter.api.advice;
+
+import es.lareira.denodo.application.domain.model.exceptions.NoPurchasesFoundException;
+import es.lareira.denodo.generated.model.ErrorDTO;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error. Please contact the administrator.";
+
+    @ExceptionHandler(NoPurchasesFoundException.class)
+    public ResponseEntity<ErrorDTO> handleNoPurchasesFoundException(NoPurchasesFoundException ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.code(HttpStatus.NOT_FOUND.value());
+        errorDTO.setMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDTO> handleConstraintViolationException(ConstraintViolationException ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.code(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        errorDTO.setMessage(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorDTO);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> handleUnknownException(Exception ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.code(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorDTO.setMessage(INTERNAL_SERVER_ERROR_MESSAGE);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDTO);
+    }
+}
---
+++ src/main/java/es/lareira/denodo/application/domain/validators/aspects/ValidDateRange.java
@@ -0,0 +1,21 @@
+package es.lareira.denodo.application.domain.validators.aspects;
+
+
+import es.lareira.denodo.application.domain.validators.DateRangeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = DateRangeValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDateRange {
+
+    String message() default "Start date must be before end date";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
